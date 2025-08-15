0d470ace0b97a2301dcf2d84d24e233871ab1612
Cilla <ceciliawannberg@gmail.com>
2025-08-15 10:02:59 +0200

    Removed dummydata. Finished desgin. Finished version

--- blogpostmanager/src/sidebar.ts
+++ blogpostmanager/src/sidebar.ts
@@ -1,6 +1,5 @@
 import './css/style.css'
 import { toggleForm } from './formManager';
-import { DummyPosts } from './data';
 
 const blogpostUListEle = getBlogpostUListElement();
 const sidebar = document.querySelector<HTMLElement>('#sidebar')!;
@@ -8,14 +7,7 @@ const sidebar = document.querySelector<HTMLElement>('#sidebar')!;
 export function getBlogpostUListElement(): HTMLUListElement | null {
     return document.querySelector<HTMLUListElement>('.blogpost-list');
 }
-function addDummyPostsToList(): void{
-    DummyPosts.forEach(post => {
-        const listElement = document.createElement('li');
-        listElement.textContent=`${post.title}`;
-        blogpostUListEle?.appendChild(listElement);
-    });
-}
-addDummyPostsToList();
+
 const postBtn = document.querySelector(".new-post-btn");
 postBtn?.addEventListener("click", (e) => {
     e.preventDefault();
